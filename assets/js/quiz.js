889ca8476fc1785e9ffb04b65b2543c2560963ec
Hugh1996 <>
2023-07-13 21:54:27 +0000

    Add quiz template content

---
+++ assets/js/quiz.js
@@ -0,0 +1,24 @@
+function startQuiz() {
+
+    let container = document.getElementById("container");
+
+    container.innerHTML = "";
+
+    let quizArea = document.createElement("div");
+    quizArea.id = "quiz-area";
+
+    let question = document.createElement("h2");
+    question.textContent = "This will be the first question";
+    quizArea.appendChild(question);
+
+    let options = ["1", "2", "3", "4"];
+    options.forEach(function (option) {
+        let answer = document.createElement("button");
+        answer.className = "quiz-btn";
+        answer.textContent = option;
+        quizArea.appendChild(answer);
+    });
+
+    container.appendChild(quizArea);
+}
+
