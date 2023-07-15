1e2d863c17c772ea35a074268ddf10a0d5e562d4
Hugh1996 <>
2023-07-15 15:14:08 +0000

    Add questions to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,53 @@ function startQuiz(questions) {
 
     container.innerHTML = "";
 
-    let quizArea = document.createElement("div");
-    quizArea.id = "quiz-area";
+    for (let question of questions) {
+        let quizArea = document.createElement("div");
+        quizArea.id = "quiz-area";
 
-    let question = document.createElement("h2");
-    question.textContent = "This will be the first question";
-    quizArea.appendChild(question);
+        let questionOption = document.createElement("h2");
+        questionOption.textContent = question.question;
+        quizArea.appendChild(questionOption);
 
+        for (let option of question.options) {
+            let answer = document.createElement("button");
+            answer.className = "quiz-btn";
+            answer.textContent = option;
+            quizArea.appendChild(answer);
 
-    let options = ["1", "2", "3", "4"];
-    for (let option of options) {
-        let answer = document.createElement("button");
-        answer.className = "quiz-btn";
-        answer.textContent = option;
-        quizArea.appendChild(answer);
+        }
 
-    };
+        container.appendChild(quizArea);
 
-    container.appendChild(quizArea);
+    }
 
 }
 
+let questions = [
+    {
+        question: "Who was the first Roman Emperor?",
+        options: ["Augustus", "Julius Caesar", "Marcus Aurelius", "Constantine"]
+    },
+    {
+        question: "Which was the privileged social class in Ancient Roman Society?",
+        options: ["Publican", "Patrician", "Plebeian", "Eques"]
+    },
+    {
+        question: "What was the eastern part of the Roman Empire called?",
+        options: ["Eastern Empire", "Sasanian Empire", "Byzantine Empire", "Akkadian Empire"]
+    },
+    {
+        question: "The ancient city of Rome was built on this many hills?",
+        options: ["14", "10", "3", "7"]
+    },
+    {
+        question: "In what year did the Western Roman Empire collapse?",
+        options: ["1453 AD", "753 BC", "476 AD", "27 BC"]
+    },
+    {
+        question: "Who was the legendary founder of Rome?",
+        options: ["Hannibal", "Spartacus", "Augustus", "Romulus"]
+    }
+];
+
+startQuiz(questions);
