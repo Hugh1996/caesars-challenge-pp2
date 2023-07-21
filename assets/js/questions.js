85be38495ec1fa841e84ea226c0ed3c2741c8d39
Hugh1996 <>
2023-07-21 21:51:48 +0000

    Add question.js file and transfer questions

---
+++ assets/js/questions.js
@@ -0,0 +1,59 @@
+
+/* List of questions */
+
+let questions = [
+    {
+        question: "Who was the first Roman Emperor?",
+        options: ["Augustus", "Julius Caesar", "Marcus Aurelius", "Constantine"],
+        answer: "Augustus"
+    },
+    {
+        question: "Which was the privileged social class in Ancient Roman Society?",
+        options: ["Publican", "Patrician", "Plebeian", "Eques"],
+        answer: "Patrician"
+    },
+    {
+        question: "What was the eastern part of the Roman Empire called?",
+        options: ["Eastern Empire", "Sasanian Empire", "Byzantine Empire", "Akkadian Empire"],
+        answer: "Byzantine Empire"
+    },
+    {
+        question: "The ancient city of Rome was built on this many hills?",
+        options: ["14", "10", "3", "7"],
+        answer: "7"
+    },
+    {
+        question: "In what year did the Western Roman Empire collapse?",
+        options: ["1453 AD", "753 BC", "476 AD", "27 BC"],
+        answer: "476 AD"
+    },
+    {
+        question: "Who was the legendary founder of Rome?",
+        options: ["Hannibal", "Spartacus", "Augustus", "Romulus"],
+        answer: "Romulus"
+    },
+    {
+        question: "Which Roman Emperor named his horse senator?",
+        options: ["Caligula", "Nero", "Caracalla", "Domitian"],
+        answer: "Caligula"
+    },
+    {
+        question: "Where is Hadrian's Wall located?",
+        options: ["England", "Italy", "Spain", "Turkey"],
+        answer: "England"
+    },
+    {
+        question: "Who was the first Roman Emperor to convert to Christianity?",
+        options: ["Severus Alexander", "Constantine I", "Diocletian", "Vespasian"],
+        answer: "Constantine I"
+    },
+    {
+        question: "Under which emperor did Rome reach its zenith?",
+        options: ["Hadrian", "Nerva", "Marcus Aurelius", "Trajan"],
+        answer: "Trajan"
+    }
+
+
+];
+
+console.log(questions);
